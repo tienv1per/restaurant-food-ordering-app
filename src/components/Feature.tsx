fa9e6c5d70778437b4aaae4485932daedefb783b
tienv1per <tienv1perds@gmail.com>
2023-08-20 18:53:35 +0700

    add feature components render data

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,3 +1,4 @@
+import { featuredProducts } from '@/data';
 import Image from 'next/image';
 import React from 'react'
 
@@ -7,21 +8,26 @@ const Feature = () => {
             {/* WRAPPER */}
             <div className='w-max flex'>
                 {/* SINGLE ITEM */}
-                <div className=''>
-                    {/* IMAGE CONTAINER */}
-                    <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
-                        <Image src="" alt='' fill className='object-contain'/>
+                {featuredProducts.map((item) => (
+                    <div 
+                        className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]' 
+                        key={item.id}
+                    >
+                        {/* IMAGE CONTAINER */}
+                        {item.img && <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+                            <Image src={item.img} alt='' fill className='object-contain'/>
+                        </div>}
+                        {/* TEXT CONTAINER */}
+                        <div className='flex-1 flex flex-col items-center justify-center text-center gap-4'>
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                            <p className="p-4 2xl:p-8">{item.desc}</p>
+                            <span className="text-xl font-bold">${item.price}</span>
+                            <button className='bg-red-500 text-white p-2 rounded-md'>
+                                Add To Cart
+                            </button>
+                        </div>
                     </div>
-                    {/* TEXT CONTAINER */}
-                    <div className='flex-1 flex flex-col items-center justify-center text-center gap-4'>
-                        <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl"></h1>
-                        <p className="p-4 2xl:p-8"></p>
-                        <span className="text-xl font-bold">$123</span>
-                        <button className='bg-red-500 text-white p-2 rounded-md'>
-                            Add To Cart
-                        </button>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
