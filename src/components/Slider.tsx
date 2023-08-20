8a879e9084d7424545a0b78004a5980c54aa8946
tienv1per <tienv1perds@gmail.com>
2023-08-20 15:15:05 +0700

    add auto slider effect

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const data = [
@@ -21,18 +22,27 @@ const data = [
 ];
 
 const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentSlide((prev) => (prev === data.length - 1) ? (0) : (prev + 1));
+        }, 2000);
+        // cleanup function
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
             {/* TEXT CONTAINER */}
-            <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-                <h1 className='text-5xl text-center uppercase p-4'>
-                    Test
+            <div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
+                <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+                    {data[currentSlide].title}
                 </h1>
-                <button className='bg-red-500 text-white py-4 px-8 md:text-6xl xl:text-7xl'>Order Now</button>
+                <button className='bg-red-500 text-white py-4 px-8'>Order Now</button>
             </div>
             {/* IMAGE CONTAINER */}
-            <div className='w-full h-1/2 relative'>
-                <Image src="/slide1.png" alt='' fill className='object-cover'/>
+            <div className='w-full flex-1 relative'>
+                <Image src={data[currentSlide].image} alt='' fill className='object-cover'/>
             </div>
         </div>
     )
