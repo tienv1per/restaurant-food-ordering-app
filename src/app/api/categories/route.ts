13245533498f4cd2635e9eb0c54e829c664707ee
tienv1per <tienv1perds@gmail.com>
2023-08-21 23:05:56 +0700

    add api endpoint categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server"
 
-export const GET = () => {
-    return new NextResponse("Hello", {status: 200});
+const prisma = new PrismaClient();
+
+// fetching all categories
+export const GET = async() => {
+    try {
+        const categories = await prisma.category.findMany();
+        return new NextResponse(JSON.stringify(categories), {status: 200})
+    } catch (error) {
+        console.log(error);
+        return new NextResponse(JSON.stringify({message: "Something went wrong"}), {status: 500});
+    }
+    
 }
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,12 +1,23 @@
-import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import Link from "next/link";
 
-export default function Page() {
+const getData = async() => {
+	const res = await fetch("http://localhost:3000/api/categories", {
+		cache: "no-store"
+	});
+	if(!res.ok) {
+		throw new Error("Failed");
+	}
+	return res.json();
+}
+
+const Page =  async() => {
+	const menu: MenuType = await getData();
     return (
         <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
 			{menu.map((category) => (
 				<Link 
-					href={`menu/${category.slug}`} 
+					href={`/menu/${category.slug}`} 
 					key={category.id} 
 					className="w-full h-1/3 bg-cover p-8 md:h-1/2"
 					style={{ backgroundImage: `url(${category.img})` }}
@@ -21,3 +32,5 @@ export default function Page() {
         </div>
     )
 };
+
+export default Page;
