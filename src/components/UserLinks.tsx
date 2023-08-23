4d979f512a7935756a3a954ad3179f0c7a844aaa
tienv1per <tienv1perds@gmail.com>
2023-08-23 22:05:01 +0700

    add login with google using nextauth

---
+++ src/components/UserLinks.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+const UserLinks = () => {
+    const user = false;
+    const { data, status } = useSession();
+    return (
+        <div>
+            {status==="authenticated" ? (
+                <div>
+                    <Link href="/orders">Orders</Link>
+                    <span className="ml-4 cursor-pointer" onClick={() => signOut()}>Logout</span>
+                </div>
+            ) : (
+                <Link href="/login">Login</Link>
+            )}
+        </div>
+    )
+};
+
+export default UserLinks;
---
+++ src/utils/auth.ts
@@ -0,0 +1,12 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+    ],
+};
